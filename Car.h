29e102a08c49afd0ddff8f3f9459dc9fff525cea
NeedForSpeed1 <sarah.holzworth@gmail.com>
2023-05-24 10:02:21 -0500

    added more position attributes for car object

--- Car.h
+++ Car.h
@@ -9,17 +9,26 @@ class Car
     void setMass(float m);
     void setForce(VectorClass f);
     void setVelocity(VectorClass v);
-    void setPosition(VectorClass p);
+    void setPosition_FD(VectorClass p);
+    void setPosition_FP(VectorClass p);
+    void setPosition_RD(VectorClass p);
+    void setPosition_RP(VectorClass p);
     void setAccel(VectorClass a);
     float getMass();
     VectorClass getForce();
     VectorClass getVelocity();
-    VectorClass getPos();
+    VectorClass getPos_FD();
+    VectorClass getPos_FP();
+    VectorClass getPos_RD();
+    VectorClass getPos_RP();
     VectorClass getAccel();
 
     private:
     float car_mass;
-    VectorClass car_pos;
+    VectorClass car_pos_front_driver;
+    VectorClass car_pos_front_passenger;
+    VectorClass car_pos_rear_driver;
+    VectorClass car_pos_rear_passenger;
     VectorClass car_velocity;
     VectorClass car_accel;
     VectorClass car_force;
--- Car_Car_Res.cpp
+++ Car_Car_Res.cpp
@@ -3,4 +3,22 @@
 void Car_Car_Res::Collide(Car c1, Car c2)
 {
     //add in sequence of conditions and effects here
+    bool collision = false; //just a filler for now until tie in coll_detection
+
+    if (collision)
+    {
+        //c1 hits c2 in center while c2 at rest
+        
+
+        //c1 hits c2 in center while c2 not at rest
+            //c1 has greater velocity
+            //c2 has greater velocity
+            //equal velocity
+        
+        //c1 hits c2 from bottom angle while c2 at rest
+
+        //c1 hits c2 from top angle while c2 at rest
+
+
+    }
 }
