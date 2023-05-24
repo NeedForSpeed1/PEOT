aee77d8b653a152d551efcd286a78e5a55190213
Henry Zhou <zhou96@illinois.edu>
2023-05-23 20:36:15 -0500

    Adding preliminary Matrix inversion

--- Matrix.h
+++ Matrix.h
@@ -2,6 +2,37 @@
 #define MATRIX_H
 #include "VectorClass.h"
 
+enum MatrixInverseErrorCode{
+	NONE,
+	NON_INVERTIBLE_MATRIX
+};
+
+class ThreeByThreeMatrix{
+	public:
+	
+	ThreeByThreeMatrix();
+	float getDeterminant();
+	
+	union {
+		
+	float arrayView [9];
+	
+	struct{
+      	float x0;
+		float x1;
+		float x2;
+		float x3;
+		float x4;
+		float x5;
+		float x6;
+		float x7;
+		float x8;
+    }
+	}
+	
+};
+
+
 class Matrix{
 	
 	public:
@@ -10,15 +41,24 @@ class Matrix{
 	Matrix subtract(const Matrix & other) const;
 	VectorClass multVect(VectorClass & vect) const;
 	static Matrix getIdentity();
-	//Matrix crossProduct();
 	Matrix multMatrix(const Matrix & other) const;
+	
+	static MatrixInverseErrorCode inverse(const Matrix & input, const Matrix & output);
 	
 	private:
 	
+	float getDeterminant();
+	//Return 3x3 sub matrix when a given row and column are discarded from current 4x4 matrix.
+	ThreeByThreeMatrix getSubMatrix(int row, int column);
+	
+	
 	
 	public:
-	//union {
-	//struct{
+	union {
+		
+	float arrayView [16];
+	
+	struct{
       	float x0;
 		float x1;
 		float x2;
@@ -35,11 +75,11 @@ class Matrix{
 		float x13;
 		float x14;
 		float x15;
-
-
-    //}
-	//}
+    }
+	}
 };
+
+
 
 
 
