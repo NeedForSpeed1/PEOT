0623e874866c8edddc5be68283bd48382c8fd562
Bryan <thisnotbryan@gmail.com>
2023-06-06 22:40:52 -0500

    Adding opencv

---
+++ build/CMakeFiles/PEOT.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/thisn/OneDrive/Desktop/Projects/PEOT/PEOT/Particle.cpp" "CMakeFiles/PEOT.dir/Particle.cpp.obj" "gcc" "CMakeFiles/PEOT.dir/Particle.cpp.obj.d"
+  "C:/Users/thisn/OneDrive/Desktop/Projects/PEOT/PEOT/main.cpp" "CMakeFiles/PEOT.dir/main.cpp.obj" "gcc" "CMakeFiles/PEOT.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PEOT.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PEOT.dir/Particle.cpp.obj"
+  "CMakeFiles/PEOT.dir/Particle.cpp.obj.d"
+  "CMakeFiles/PEOT.dir/main.cpp.obj"
+  "CMakeFiles/PEOT.dir/main.cpp.obj.d"
+  "PEOT.exe"
+  "PEOT.exe.manifest"
+  "PEOT.pdb"
+  "libPEOT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PEOT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
